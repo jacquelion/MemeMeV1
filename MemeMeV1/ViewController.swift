9646caa950997a3e50e835aa72814fcf3149f5dd
jacquelion <coderjac@gmail.com>
2016-03-04 16:53:12 -0800

    Initial Commit

---
+++ MemeMeV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMeV1
+//
+//  Created by Jacqueline Sloves on 3/4/16.
+//  Copyright © 2016 Jacqueline Sloves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
